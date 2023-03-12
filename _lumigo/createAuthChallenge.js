c1e7755850cf49d968e1e4fcc829876d43dc6423
theburningmonk <theburningmonk@gmail.com>
2023-03-12 02:37:56 +0100

    feat: removed lumigo

--- _lumigo/createAuthChallenge.js
+++
@@ -1,7 +0,0 @@
-
-const tracer = require("@lumigo/tracer")({
-	token:'t_7afe5952d7644bd48a2f5'
-});
-const handler = require('../functions/create-auth-challenge').handler;
-
-module.exports.handler = tracer.trace(handler);
